a0fe42a7b2356518236ff47a212a0a72dc6d686d
Medet Ahmetson <ahmetson@zoho.com>
2023-09-26 16:48:08 +0700

    messages are implementing standard Stringer interface

--- data_type/bytes.go
+++ data_type/bytes.go
@@ -2,9 +2,9 @@
 //
 // Supported data types are:
 //   - Queue is the list where the new element is added to the end,
-//     but when element is taken its taken from the top.
+//     but when an element is taken its taken from the top.
 //     Queue doesn't allow addition of any kind of element. All elements should have the same type.
-//   - key_value different kind of maps
+//   - Key_value different kinds of maps
 //   - serialize functions to serialize any structure to the bytes and vice versa.
 package data_type
 
